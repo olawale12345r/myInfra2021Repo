819b1e0f97fed30433b0f41b6dca11f90b7e3a5c
olawale12345r <64484864+olawale12345r@users.noreply.github.com>
2024-10-18 00:19:11 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-/*
+
 variable "aws_region" {
        description = "The AWS region to create things in." 
        default     = "us-east-2" 
@@ -27,8 +27,27 @@ variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance" 
     default     = "ami-0ea3c35c5c3284d82" 
 }
-*/
-variable "github_token" {
-  type      = string
-  sensitive = true
+
+variable "versioning" {
+    type        = bool
+    description = "(Optional) A state of versioning."
+    default     = true
+}
+variable "acl" {
+    type        = string
+    description = " Defaults to private "
+    default     = "private"
+}
+variable "bucket_prefix" {
+    type        = string
+    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
+    default     = "my-s3bucket-"
+}
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
 }
